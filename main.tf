a8f66bc323618f2dc038d498a1cb0e7178e57f7f
Drew Leske <drew.leske@computecanada.ca>
2018-08-01 00:18:46 -0700

    Fix typos, copy/paste errors, self references

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # get local variables
 locals {
   lbr_count = "${length(var.lbr_names)}"
-  pp_count  = :${length(var.pp_names)}"
+  pp_count  = "${length(var.pp_names)}"
 }
 
 # This is used to remove and recreate the SSH config
@@ -31,12 +31,12 @@ data "openstack_images_image_v2" "lbr_image" {
 }
 
 # floating IP resource for node
-resource "openstack_networking_floatingip_v2" "fip" {
+resource "openstack_networking_floatingip_v2" "lbr_fip" {
   count = "${local.lbr_count}"
   pool = "${var.lbr_spec["floating_ip_pool"]}"
 }
 
-# Load Balancer/Router node: persistent with root disk and floating IP
+# node definition
 resource "openstack_compute_instance_v2" "lbr_node" {
   count           = "${local.lbr_count}"
   name            = "${element(var.lbr_names, count.index)}"
@@ -59,15 +59,15 @@ resource "openstack_compute_instance_v2" "lbr_node" {
 }
 
 # association between floating IP and node resources
-resource "openstack_compute_floatingip_associate_v2" "fip" {
+resource "openstack_compute_floatingip_associate_v2" "lbr_fip" {
   count = "${local.lbr_count}"
   instance_id = "${element(openstack_compute_instance_v2.lbr_node.*.id, count.index)}"
-  floating_ip = "${element(openstack_networking_floatingip_v2.fip.*.address, count.index)}"
+  floating_ip = "${element(openstack_networking_floatingip_v2.lbr_fip.*.address, count.index)}"
 
   # creation triggers addition of host clause to SSH config
   provisioner "local-exec" {
     # TODO: use variable for config file
-    command = "echo 'Host ${element(openstack_compute_instance_v2.lbr_node.*.name, count.index)}\n\tHostname ${element(openstack_networking_floatingip_v2.fip.*.address, count.index)}\n' >> ssh_config"
+    command = "echo 'Host ${element(openstack_compute_instance_v2.lbr_node.*.name, count.index)}\n\tHostname ${element(openstack_networking_floatingip_v2.lbr_fip.*.address, count.index)}\n' >> ssh_config"
   }
 }
 
@@ -87,13 +87,7 @@ data "openstack_images_image_v2" "pp_image" {
   name = "${var.pp_spec["image"]}"
 }
 
-# floating IP resource for node
-resource "openstack_networking_floatingip_v2" "fip" {
-  count = "${local.pp_count}"
-  pool = "${var.pp_spec["floating_ip_pool"]}"
-}
-
-# Load Balancer/Router node: persistent with root disk and floating IP
+# node definition
 resource "openstack_compute_instance_v2" "pp_node" {
   count           = "${local.pp_count}"
   name            = "${element(var.pp_names, count.index)}"
@@ -115,8 +109,8 @@ resource "openstack_compute_instance_v2" "pp_node" {
   }
 
   # creation triggers addition of host clause to SSH config
-  provision "local-exec" {
+  provisioner "local-exec" {
     # TODO: use variable for config file
-    command = "echo 'Host ${element(openstack_compute_instance_v2.pp_node.*.name, count.index)}\n\tHostname ${element(openstack_compute_instance_v2.pp_node.*.network.1.fixed_ip_v4, count.index)}\n\tProxyCommand ssh -q -W %h ${var.pp_spec["jumphost"]}\n' >> ssh_config"
+    command = "echo 'Host ${self.name}\n\tHostname ${self.network.0.fixed_ip_v4}\n\tProxyCommand ssh -q -W %h ${var.pp_spec["jumphost"]}\n' >> ssh_config"
   }
 }
